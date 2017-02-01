927393620a2d3b4ce52251e90a37d97e3fe2baec
zasipin <zasipin.it@gmail.com>
2017-02-01 23:52:49 +0300

    Update intervals in timer

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -6,13 +6,40 @@ export default class Countdown extends React.Component {
   constructor() {
     super();
     this.state = {
-      count: 0
+      count: 0,
+      countdownStatus: 'stopped'
     }
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(this.state.countdownStatus !== prevState.countdownStatus){
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        // case:
+        default:
+
+      }
+    }
+  }
+
+  startTimer(){
+    this.timer = setInterval(()=> {
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+      if (newCount === 0) {
+        clearInterval(this.timer);
+      }
+    }, 1000);
   }
 
   handleSetCountdown(seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started'
     });
   }
 
